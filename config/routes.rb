5bfaa0caad2369bbd2543d2b9d36bed0c0e74f9d
pavel <kalashnikovisme@gmail.com>
2013-10-12 19:29:23 +0400

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 MicEvents::Application.routes.draw do
+  root to: "welcome#index"
   resources :places
   resources :users
   resources :members
   resources :checkins
   resources :events
+  resource :welcome, only: :index
 end
