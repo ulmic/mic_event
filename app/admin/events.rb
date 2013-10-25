6895c4444ecb938ec08d269525ab32340946fd97
pavel <kalashnikovisme@gmail.com>
2013-10-25 04:50:03 +0400

    add admins files

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Event do
-  
+  index do
+    selectable_column
+    column :title
+    column :description
+    column :event_time
+    column :member
+    column :place
+  end
+  form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :description
+      f.input :event_time
+      f.input :member
+      f.input :place
+    end
+    f.actions
+  end
 end
--- app/admin/places.rb
+++ app/admin/places.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Place do
-  
+  index do
+    column :description
+    column :title
+  end
+  form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :description
+    end
+    f.actions
+  end
 end
