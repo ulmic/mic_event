9f5f21852606f3101438459b588a270879cb51e1
pavel <kalashnikovisme@gmail.com>
2013-11-07 06:16:26 +0400

    up coverage

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,6 +1,13 @@
 require 'test_controller_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  setup do
+    @program = create :program
+    @checkin = create :checkin
+    @event = create :event
+    @event.checkins.push @checkin
+    @event.program = @program
+  end
   test "should get welcome" do
     get :index
     assert_response :success
