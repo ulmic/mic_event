f3854a0c0d2eeb186665c6e297492e35bb16aae7
aelaa <aelaau@gmail.com>
2013-11-19 10:22:00 +0400

    micfont added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ MicEvents::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .ttf )
 
   # Expands the lines which load the assets
   config.assets.debug = true
